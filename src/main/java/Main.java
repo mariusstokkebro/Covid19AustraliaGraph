a72d2ec6a6fe0144be5111c794b2c55600c94fdb
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-10-30 09:06:51 +0100

    det cool

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,21 @@ import processing.core.PApplet;
 
 public class Main extends PApplet{
         public static void main(String[] args) { PApplet.main("Main"); }
+
+        @Override
+        public void settings() {
+                super.settings();
+                size(500,500);
+        }
+
+        @Override
+        public void setup() {
+                super.setup();
+        }
+
+        @Override
+        public void draw() {
+
+
+        }
 }
